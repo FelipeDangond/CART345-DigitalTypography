c9c0eeb341977be55e1bb6c52834e9c8abced38b
Felipe Dangond <felipe_dangond@hotmail.com>
2018-11-12 16:14:31 -0500

    aa

--- js/script.js
+++ js/script.js
@@ -1,5 +1,174 @@
+$(function()
+{
+  var video = $('body > video')[0],
+	  cvs = document.createElement('canvas'),
+	  ctx = cvs.getContext('2d');
+
+  navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+
+  navigator.getMedia({ video: !0, audio: !1 }, function(stream)
+  {
+    if(navigator.mozGetUserMedia)
+      video.mozSrcObject = stream;
+    else
+    {
+      var vu = window.URL || window.webkitURL;
+      video.src = vu.createObjectURL(stream);
+    }
+    video.play();
+  }, function(error)
+  {
+    if(window.console)
+      console.error(error);
+  });
+
+  video.addEventListener('canplay', function(ev)
+  {
+    if(!streaming)
+    {
+      height = video.videoHeight / (video.videoWidth / width);
+      video.setAttribute('width', width);
+      video.setAttribute('height', height);
+      streaming = !0;
+    }
+  }, !1);
+
+
+});
 
 
 $(document).ready(function() {
 
+
+  // dots is an array of Dot objects,
+// mouse is an object used to track the X and Y position
+   // of the mouse, set with a mousemove event listener below
+var dots = [],
+    mouse = {
+      x: 0,
+      y: 0
+    };
+
+// The Dot object used to scaffold the dots
+var Dot = function() {
+  this.x = 0;
+  this.y = 0;
+  this.node = (function(){
+    var n = document.createElement("div");
+    n.className = "trail";
+    document.body.appendChild(n);
+    return n;
+  }());
+};
+// The Dot.prototype.draw() method sets the position of
+  // the object's <div> node
+Dot.prototype.draw = function() {
+  this.node.style.left = this.x + "px";
+  this.node.style.top = this.y + "px";
+};
+
+// Creates the Dot objects, populates the dots array
+for (var i = 0; i < 12; i++) {
+  var d = new Dot();
+  dots.push(d);
+}
+
+// This is the screen redraw function
+function draw() {
+  // Make sure the mouse position is set everytime
+    // draw() is called.
+  var x = mouse.x,
+      y = mouse.y;
+
+  // This loop is where all the 90s magic happens
+  dots.forEach(function(dot, index, dots) {
+    var nextDot = dots[index + 1] || dots[0];
+
+    dot.x = x;
+    dot.y = y;
+    dot.draw();
+    x += (nextDot.x - dot.x) * .6;
+    y += (nextDot.y - dot.y) * .6;
+
+  });
+}
+
+addEventListener("mousemove", function(event) {
+  //event.preventDefault();
+  mouse.x = event.pageX;
+  mouse.y = event.pageY;
 });
+
+// animate() calls draw() then recursively calls itself
+  // everytime the screen repaints via requestAnimationFrame().
+function animate() {
+  draw();
+  requestAnimationFrame(animate);
+}
+
+// And get it started by calling animate().
+animate();
+
+
+
+
+      $("h1").on({
+          mouseover:function(){
+              $(this).css({
+                  left:(Math.random()*100)+"%",
+                  top:(Math.random()*100)+"%",
+              });
+          }
+      });
+
+
+      animateDiv();
+
+});
+
+
+$(document).ready(function(){
+    animateDiv('.a');
+    animateDiv('.b');
+    animateDiv('.c');
+    animateDiv('.d');
+    animateDiv('.e');
+    animateDiv('.f');
+    animateDiv('.g');
+    animateDiv('.h');
+});
+
+function makeNewPosition(){
+
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() -100;
+    var w = $(window).width() - 100;
+
+    var nh = Math.floor(Math.random() * h) + 25;
+    var nw = Math.floor(Math.random() * w);
+
+    return [nh,nw];
+
+}
+
+function animateDiv(myclass){
+    var newq = makeNewPosition();
+    $(myclass).animate({ top: newq[0], left: newq[1] }, 1000,   function(){
+      animateDiv(myclass);
+    });
+
+};
+
+
+$( function() {
+$( '.a'  ).draggable();
+$( '.b'  ).draggable();
+$( '.c'  ).draggable();
+$( '.d'  ).draggable();
+$( '.e'  ).draggable();
+$( '.f'  ).draggable();
+$( '.g'  ).draggable();
+$( '.h'  ).draggable();
+
+
+} );
